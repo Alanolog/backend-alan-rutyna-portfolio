78ab9ceee5c8cc20b521702f5c31e0f5653b638c
Alanolog <alanrutyna2003@gmail.com>
2022-12-03 17:34:58 +0100

    Task Schema

---
+++ models/Task.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const TaskSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "must provide name"],
+      trim: true,
+      maxlength: [20, "name can not be more than 20 characters"],
+    },
+    description: {
+      type: String,
+      required: [true, "Please provide task description"],
+    },
+    isDone: {
+      type: Boolean,
+      default: false,
+    },
+    createdBy: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: [true, "Please provide user"],
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Task", TaskSchema);
