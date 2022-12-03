744d8ad84869f53430f8f4f27a8ac23919751e43
Alanolog <alanrutyna2003@gmail.com>
2022-12-03 19:55:11 +0100

    added delete and update task

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,6 +1,6 @@
 const Task = require("../models/Task");
 const { StatusCodes } = require("http-status-codes");
-const { NotFoundError } = require("../errors/index");
+const { BadRequestError, NotFoundError } = require("../errors/index");
 
 const createTask = async (req, res) => {
   req.body.createdBy = req.user.userId;
@@ -8,10 +8,37 @@ const createTask = async (req, res) => {
   res.status(StatusCodes.CREATED).json({ task });
 };
 const deleteTask = async (req, res) => {
-  res.send("Task deleted");
+  const {
+    user: { userId },
+    params: { id: taskId },
+  } = req;
+  const task = await Task.findByIdAndDelete({ _id: taskId, createdBy: userId });
+  if (!task) {
+    throw new NotFoundError(`No task with id ${taskId}`);
+  }
+  res.status(StatusCodes.OK).send();
 };
 const updateTask = async (req, res) => {
-  res.send("Task updated");
+  const {
+    body: { description, name },
+    user: { userId },
+    params: { id: taskId },
+  } = req;
+  if (description === "" || name === "") {
+    throw new BadRequestError("description and name fields cannot be empty");
+  }
+  const task = await Task.findByIdAndUpdate(
+    {
+      _id: taskId,
+      createdBy: userId,
+    },
+    req.body,
+    { new: true, runValidators: true }
+  );
+  if (!task) {
+    throw new NotFoundError(`There is no task with ${taskId}`);
+  }
+  res.status(StatusCodes.OK).json({ task });
 };
 const getSingleTask = async (req, res) => {
   const {
