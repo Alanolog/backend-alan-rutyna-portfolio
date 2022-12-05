e29b948b3144f1c0789bd4d9bf8af14a841080a3
Alanolog <alanrutyna2003@gmail.com>
2022-12-05 12:10:25 +0100

    swaggerUI added

--- app.js
+++ app.js
@@ -6,10 +6,17 @@ const connectDB = require("./db/connect");
 const authRouter = require("./routes/auth");
 const tasksRouter = require("./routes/tasks");
 
+//security
 const helmet = require("helmet");
 const cors = require("cors");
 const xss = require("xss-clean");
 const rateLimiter = require("express-rate-limit");
+
+//swagger
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
+
 // error handler
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
@@ -23,12 +30,13 @@ app.use(cors());
 app.use(xss());
 
 app.use(express.json());
-// extra packages
 
 // routes
 app.get("/", (req, res) => {
-  res.send("<h1>portfolio api</h1>");
+  res.send('<h1>portfolio api</h1><a href="/api-docs">Documentation</a>');
 });
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/tasks", authenticationMiddleware, tasksRouter);
 
