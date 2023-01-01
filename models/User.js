eca6f948dcb45c3267ffafe3a764d6bf4ff1386e
Alanolog <alanrutyna2003@gmail.com>
2023-01-01 09:49:25 +0100

    username unique requirement deleted

--- models/User.js
+++ models/User.js
@@ -5,7 +5,6 @@ const UserSchema = new mongoose.Schema({
   username: {
     type: String,
     required: [true, "Please provide name!"],
-    unique: true,
     minlength: 3,
     maxlength: 20,
   },
